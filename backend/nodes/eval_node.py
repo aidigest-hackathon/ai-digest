a1ae202d0911a1cb9fea091e3c6591ddc923cc65
VitalC-3026 <1184649102@qq.com>
2024-08-11 16:26:42 -0700

    eval node

--- backend/nodes/eval_node.py
+++ backend/nodes/eval_node.py
@@ -1,53 +1,72 @@
 from langsmith import Client
 from langsmith.schemas import Run, Example
-from langsmith.evaluation import evaluate
+from langsmith.evaluation import evaluate, LangChainStringEvaluator
 import openai
 from langsmith.wrappers import wrap_openai
 from dotenv import load_dotenv
-load_dotenv()
-client = Client()
+from datetime import datetime
 
-# Define dataset: these are your test cases
-dataset_name = "Research Evals Dataset"
-dataset_id = "faba1867-6268-410e-a22c-d56def8a87cc"
-# dataset = client.create_dataset(dataset_name, description="Research Evals Dataset.")
-client.create_examples(
-    inputs=[
-        {"question": "what can you help with today?"},
-        {"question": "is the attached research summary looking good?"},
-    ],
-    outputs=[
-        {"must_mention": ["helpfulness", "Coherence", "Relevance"]},
-        {"must_mention": ["helpfulness", "Coherence"]},
-    ],
-    dataset_id=dataset_id,
-)
+load_dotenv()
 
 # Define AI system
 openai_client = wrap_openai(openai.Client())
 
-def predict(inputs: dict) -> dict:
-    messages = [{"role": "user", "content": inputs["question"]}]
-    response = openai_client.chat.completions.create(messages=messages, model="gpt-3.5-turbo")
-    return {"output": response}
 
-# Define evaluators
-def must_mention(run: Run, example: Example) -> dict:
-    prediction = run.outputs.get("output") or ""
-    required = example.outputs.get("must_mention") or []
-    score = all(phrase in prediction for phrase in required)
-    return {"key":"must_mention", "score": score}
+def generate_results(inputs: str) -> dict:
+    # TODO: call function to generat the summary
+    summary = "This is a summary of the research paper"
+    return {"output": summary}
 
-experiment_results = evaluate(
-    predict, # Your AI system
-    data=dataset_name, # The data to predict and grade over
-    evaluators=[must_mention], # The evaluators to score the results
-    experiment_prefix="research-digest-generator", # A prefix for your experiment names to easily identify them
-    metadata={
-      "version": "1.0.0",
-    },
-)
 
-if __name__ == "__main__":
-    print(experiment_results)
-    print("Done!")
+def evaluate_summary(inputs):
+
+    client = Client()
+    dataset_name = "Research Paper Summary" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    dataset = client.create_dataset(dataset_name=dataset_name)
+
+    client.create_examples(inputs=inputs, dataset_id=dataset.id)
+
+    brevity_evaluator = LangChainStringEvaluator(
+        "score_string",
+        config={
+            "criteria": {
+                "brevity": "How concise, coherent and well-structured is the summary on a scale of 1-10? It should be no more than 7 sentences." 
+            },
+            "normalize_by": 10,
+        }  
+    )
+
+    fluency_evaluator = LangChainStringEvaluator(
+        "score_string",
+        config={
+            "criteria": {
+                "fluency": "Can the summary be understood in isolation, without reference to the original paper on a scale of 1-10?" 
+            },
+            "normalize_by": 10,
+        }  
+    )
+
+    repeatness_evaluator = LangChainStringEvaluator(
+        "score_string",
+        config={
+            "criteria": {
+                "repeatness": "Is the summary unique and does it paraphrase the abstract instead of just repeating the words on a scale of 1-10?" 
+            },
+            "normalize_by": 10,
+        }  
+    )
+
+
+    results = evaluate(
+        generate_results,
+        data=dataset_name,
+        evaluators=[brevity_evaluator, fluency_evaluator, repeatness_evaluator],
+        experiment_prefix="research-digest-generator", # A prefix for your experiment names to easily identify them
+        metadata={
+        "version": "1.0.0",
+        },
+    )
+
+    for result in results:
+        for eval_result in result["evaluation_results"]:
+            print(eval_result.get("key"), eval_result.get("score"))
